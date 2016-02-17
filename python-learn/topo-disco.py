1174c4fc84f93cdc1fb087881f8ae914bdf2e252
ximenchuixue <mshmayoushi@gmail.com>
2016-02-17 11:37:31 +0800

    Create topo-disco.py

---
+++ python-learn/topo-disco.py
@@ -0,0 +1,14 @@
+import nmap, os
+
+nm = nmap.PortScanner()
+nm.scan(hosts = '192.168.1.110-116', arguments = '-PU -sn')
+for host in nm.all_hosts():
+    print "host: %s" % host
+print "FIRST"
+hostlist = ' '.join(nm.all_hosts())
+nm.scan(hosts = hostlist, arguments = '-PU -sn')
+
+for host in nm.all_hosts():
+    print "host: %s" % host
+
+os.system('./runsnmp')
